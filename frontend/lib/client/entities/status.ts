c90ef49f3db564a414607d395fea8be315b544bb
AkiraFukushima <h3.poteto@gmail.com>
2019-08-11 16:22:35 +0900

    refs #3 Add branch and status types

---
+++ frontend/lib/client/entities/status.ts
@@ -0,0 +1,3 @@
+export type Status = {
+  status: string
+}
--- frontend/lib/client/index.ts
+++ frontend/lib/client/index.ts
@@ -1,8 +1,10 @@
 import Yadockeri from './yadockeri'
 import { Project } from './entities/project'
+import { Branch } from './entities/branch'
+import { Status } from './entities/status'
 import { GitHubRepo } from './entities/github_repo'
 import { GitHubBranch } from './entities/github_branch'
 
-export { Project, GitHubRepo, GitHubBranch }
+export { Project, Branch, Status, GitHubRepo, GitHubBranch }
 
 export default Yadockeri
