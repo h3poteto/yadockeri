8dc655d9e7ae265456f24aec35bfeec06246af9c
AkiraFukushima <h3.poteto@gmail.com>
2019-08-11 22:23:12 +0900

    refs #17 [golint] Fix url to capitalize

--- app/controllers/projects_controller.go
+++ app/controllers/projects_controller.go
@@ -20,7 +20,7 @@ type NewProjectForm struct {
 	BaseURL           string                  `json:"base_url" form:"base_url" valid:"required,stringlength(1|255)"`
 	RepositoryOwner   string                  `json:"repository_owner" form:"repository_owner" valid:"required,stringlength(1|255)"`
 	RepositoryName    string                  `json:"repository_name" form:"repository_name" valid:"required,stringlength(1|255)"`
-	HelmRepositoryUrl string                  `json:"helm_repository_url" form:"helm_repository_url" valid:"required,stringlength(1|255)"`
+	HelmRepositoryURL string                  `json:"helm_repository_url" form:"helm_repository_url" valid:"required,stringlength(1|255)"`
 	HelmDirectoryName string                  `json:"helm_directory_name" form:"helm_directory_name" valid:"required,stringlength(1|255)"`
 	Namespace         string                  `json:"namespace" form:"namespace" valid:"required,stringlength(1|255)"`
 	ValueOptions      []*values.OverrideValue `json:"value_options" form:"value_options" valid:"-"`
@@ -53,7 +53,7 @@ func (p *Projects) Create(c echo.Context) error {
 		return err
 	}
 
-	proj, err := project.CreateProject(userID, newProjectForm.Title, newProjectForm.BaseURL, newProjectForm.RepositoryOwner, newProjectForm.RepositoryName, newProjectForm.HelmRepositoryUrl, newProjectForm.HelmDirectoryName, newProjectForm.Namespace, newProjectForm.ValueOptions)
+	proj, err := project.CreateProject(userID, newProjectForm.Title, newProjectForm.BaseURL, newProjectForm.RepositoryOwner, newProjectForm.RepositoryName, newProjectForm.HelmRepositoryURL, newProjectForm.HelmDirectoryName, newProjectForm.Namespace, newProjectForm.ValueOptions)
 	if err != nil {
 		return err
 	}
