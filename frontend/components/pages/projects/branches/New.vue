19e5eb6b84b8e9c4e1f68903b01381ff4662020f
AkiraFukushima <h3.poteto@gmail.com>
2019-08-12 01:57:23 +0900

    refs #17 Add validation for new branch form

--- frontend/components/pages/projects/branches/New.vue
+++ frontend/components/pages/projects/branches/New.vue
@@ -1,33 +1,43 @@
 <template>
   <div>
-    <el-form ref="form" label-width="150px">
-      <el-form-item label="Branch">
-        <el-select
-          :value="selectedBranch"
-          filterable
-          placeholder="Select"
-          :loading="loadingGithubBranch"
-          @change="changeBranch"
-        >
-          <el-option
-            v-for="item in githubBranches"
-            :key="item.name"
-            :label="item.name"
-            :value="item.name"
+    <ValidationObserver ref="observer">
+      <el-form ref="form" label-width="150px" slot-scope="{ validate }">
+        <ValidationProvider rules="required" name="selectedBranch">
+          <el-form-item
+            label="Branch"
+            slot-scope="{ errors }"
+            :error="errors[0]"
           >
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">Create</el-button>
-      </el-form-item>
-    </el-form>
+            <el-select
+              v-model="selectedBranch"
+              filterable
+              placeholder="Select"
+              :loading="loadingGithubBranch"
+            >
+              <el-option
+                v-for="item in githubBranches"
+                :key="item.name"
+                :label="item.name"
+                :value="item.name"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </ValidationProvider>
+        <el-form-item>
+          <el-button type="primary" @click="validate().then(onSubmit)"
+            >Create</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </ValidationObserver>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue'
 import { createNamespacedHelpers } from 'vuex'
+import { ValidationObserver, ValidationProvider } from 'vee-validate'
 
 const { mapState, mapActions } = createNamespacedHelpers(
   'pages/projects/branches/new'
@@ -35,24 +45,34 @@ const { mapState, mapActions } = createNamespacedHelpers(
 
 export default Vue.extend({
   name: 'new',
+  components: {
+    ValidationObserver,
+    ValidationProvider,
+  },
   props: ['project_id'],
   computed: {
-    ...mapState(['loadingGithubBranch', 'githubBranches', 'selectedBranch']),
+    ...mapState(['loadingGithubBranch', 'githubBranches']),
+    selectedBranch: {
+      get() {
+        return this.$store.state.pages.projects.branches.new.selectedBranch
+      },
+      set(value) {
+        this.changeBranch(value)
+      },
+    },
   },
   async mounted() {
-    const project = await this.fetchProject(this.project_id).catch(
-      (err: Error) => {
-        console.error(err)
-        this.$message({
-          message: 'Failed to get the project',
-          type: 'error',
-        })
-      }
-    )
+    const project = await this.fetchProject(this.project_id).catch(err => {
+      console.error(err)
+      this.$message({
+        message: 'Failed to get the project',
+        type: 'error',
+      })
+    })
     await this.fetchGithubBranches({
       owner: project.repository_owner,
       repo: project.repository_name,
-    }).catch((err: Error) => {
+    }).catch(err => {
       console.error(err)
       this.$message({
         message: 'Failed to get github branches',
@@ -68,7 +88,7 @@ export default Vue.extend({
       'submit',
     ]),
     async onSubmit() {
-      await this.submit().catch((err: Error) => {
+      await this.submit().catch(err => {
         console.error(err)
         this.$message({
           message: 'Failed to create a new branch',
