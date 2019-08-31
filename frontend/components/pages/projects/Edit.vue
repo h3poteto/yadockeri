816432593468392f6fdba56110ba56be4c08401a
AkiraFukushima <h3.poteto@gmail.com>
2019-08-31 22:12:09 +0900

    refs #21 Add label for override variables

--- frontend/components/pages/projects/Edit.vue
+++ frontend/components/pages/projects/Edit.vue
@@ -45,7 +45,7 @@
                 <el-input v-model="namespace" placeholder="default"></el-input>
               </el-form-item>
             </ValidationProvider>
-            <el-form-item label="Override Variables">
+            <el-form-item label="Override Values">
               <ul class="value-list">
                 <li class="value" v-for="(value, id) in values" :key="id">
                   <el-input
