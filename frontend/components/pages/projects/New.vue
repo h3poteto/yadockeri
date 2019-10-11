de17a306607850743bb860facfbced9dc0127e26
AkiraFukushima <h3.poteto@gmail.com>
2019-10-11 14:34:53 +0900

    refs #15 Change placeholder in project form to override image.tag with commit SHA1

--- frontend/components/pages/projects/New.vue
+++ frontend/components/pages/projects/New.vue
@@ -85,7 +85,7 @@
                 <el-input
                   :value="value.key"
                   @input="v => updateValueKey({ newKey: v, key: value.key })"
-                  placeholder="Override key"
+                  placeholder="image.tag"
                   size="small"
                 ></el-input>
                 <div>:</div>
@@ -94,7 +94,7 @@
                   @input="
                     v => updateValueOption({ newOption: v, key: value.key })
                   "
-                  placeholder="Override value"
+                  :placeholder="'{{.CommitSHA1}}'"
                   size="small"
                 ></el-input>
                 <el-button
